c9a64d02eaf80022ff7f82fe0f163f3edcaea123
David Ricardo Hernandez <ing.david.r.hernandez@gmail.com>
2026-03-03 13:49:33 +0100

    fear: add of keyboard lectuere

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,66 @@
-mod game;
 
+use std::io::{stdout, Write};
+use std::time::Duration;
+use crossterm::event::{read, Event, KeyEventKind, KeyCode, poll};
+mod game;
+ 
+ 
 fn main() {
-    println!("Squash RL");
+ 
+    let mut state = game::GameState::new();
+    let _ = crossterm::terminal::enable_raw_mode().unwrap();
+    state.active = true;
+    
+    'a: loop 
+    {            
+        while poll(Duration::from_millis(0)).unwrap()
+        {
+            match read().unwrap() 
+            {
+                Event::Key(event) => 
+                {
+                if event.kind == KeyEventKind::Press {
+                    
+                    match event.code 
+                    {
+                        KeyCode::Backspace => 
+                        {
+                            break 'a;
+                        }
+                        KeyCode::Left =>
+                        {
+                            state.move_racket(game::Action::Left);                            
+                        }
+                        KeyCode::Right =>
+                        {
+                            state.move_racket(game::Action::Right);
+                        }
+                        _ => continue,
+                    }
+                }
+            }
+            _ => continue,
+            }
+        }
+        state.update();
+        draw(&state);
+        if !state.active
+        {
+            break 'a;
+        }
+        std::thread::sleep(Duration::from_millis(50));
+    }
+    let _ = crossterm::terminal::disable_raw_mode();
+}
+
+
+fn draw(state: &game::GameState) {
+    // limpiar pantalla
+    print!("\x1B[2J\x1B[1;1H");
+    // imprimir score
+    println!("Score: {}", state.score);
+    // imprimir posición de la pelota (debug)
+    println!("Ball: ({}, {})", state.ball_pos.x, state.ball_pos.y);
+    // imprimir posición de la raqueta
+    println!("Racket: ({},{}), rang({} , {})", state.racket.racket_position.x,  state.racket.racket_position.y,  state.racket.racket_position.x - 10,  state.racket.racket_position.x + 10);
 }
