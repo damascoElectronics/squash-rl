370e5f7effce0206fd3573f029ca305838ba0811
David Ricardo Hernandez <ing.david.r.hernandez@gmail.com>
2026-03-04 13:38:45 +0100

    fix: change to pub WindowState struct

--- src/game/render.rs
+++ src/game/render.rs
@@ -1,7 +1,7 @@
 use minifb::{Window, WindowOptions};
 const FPS: usize = 60;	
 
-struct WindowState
+pub struct WindowState
 {
     buffer: Vec<u32>,
     width: usize,
@@ -46,12 +46,12 @@ impl WindowState
         let ball_pos_y:usize = state.ball_pos.y as usize;
         let racket_position_x:usize = state.racket.racket_position.x as usize;
         let racket_position_y:usize = state.racket.racket_position.y as usize;
-        let racket_length:usize = (state.racket.racket_length/2) as usize;
+        let racket_length:usize = (state.racket.length/2) as usize;
 
         self.buffer.fill(0);
         self.buffer[ball_pos_y * self.width + ball_pos_x] = 0x00FFFFFF;
 
-         for i in  racket_position_x - racket_length..racket_position_x + racket_length  {
+         for i in  (racket_position_x - racket_length)..(racket_position_x + racket_length) {
             self.buffer[racket_position_y * self.width + i] = 0x00FFFFFF;
         }
 
