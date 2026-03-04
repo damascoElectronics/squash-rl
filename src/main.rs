137134a40188634d2e15a31f8bfe528fcba1c0d2
David Ricardo Hernandez <ing.david.r.hernandez@gmail.com>
2026-03-04 16:05:13 +0100

    feat: implemenetation of the graphical interface with minifb

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,41 @@
 
 use std::io::{stdout, Write};
 use std::time::Duration;
-use crossterm::event::{read, Event, KeyEventKind, KeyCode, poll};
 mod game;
  
 fn main() {
  
     let mut state = game::GameState::new();
-    let _ = crossterm::terminal::enable_raw_mode().unwrap();
+    let mut window = game::render::WindowState::new();
     state.active = true;
     
-    'a: loop 
-    {            
-        while poll(Duration::from_millis(0)).unwrap()
+    while window.window.is_open()
+    {
+        match game::input::read_input(&window.window)
         {
-            match game::input::read_input()
+            game::input::Action::Stop => 
             {
-                game::input::Action::Stop => 
-                {
-                    break 'a;
-                }
-                game::input::Action::Left =>
-                {
-                    state.move_racket(game::Action::Left);                            
-                }
-                game::input::Action::Right =>
-                {
-                    state.move_racket(game::Action::Right);
-                }
-                _ => continue,
+                break;
             }
+            game::input::Action::Left =>
+            {
+                state.move_racket(game::Action::Left);                            
+            }
+            game::input::Action::Right =>
+            {
+                state.move_racket(game::Action::Right);
+            }
+            _ => {},
         }
+    
         state.update();
-        draw(&state);
+        window.draw(&state);
         if !state.active
         {
-            break 'a;
+            break;
         }
-        std::thread::sleep(Duration::from_millis(50));
     }
-    let _ = crossterm::terminal::disable_raw_mode();
+
 }
 
 
-fn draw(state: &game::GameState) {
-    // limpiar pantalla
-    print!("\x1B[2J\x1B[1;1H");
-    // imprimir score
-    println!("Score: {}", state.score);
-    // imprimir posición de la pelota (debug)
-    println!("Ball: ({}, {})", state.ball_pos.x, state.ball_pos.y);
-    // imprimir posición de la raqueta
-    println!("Racket: ({},{}), rang({} , {})", state.racket.racket_position.x,  state.racket.racket_position.y,  state.racket.racket_position.x - 10,  state.racket.racket_position.x + 10);
-}
